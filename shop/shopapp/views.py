f69f07d87d17cc2ecb05d8a50e7dd4e21fbb7673
userartem <gumilmo@yandex.ru>
2022-04-27 23:47:23 +0300

    queryies filters

--- shop/shopapp/views.py
+++ shop/shopapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, View
+from django.db.models import Avg, Max, Min
 from .models import AnyShoes, Category, LatestProducts, Season, Gender
 
 class BaseView(View):
@@ -9,13 +10,29 @@ class BaseView(View):
         products = AnyShoes.objects.all()
         season = list(Season.objects.all())
         gender = list(Gender.objects.all())
+        max_val = AnyShoes.objects.aggregate(Max('price'))
         print(products[0])
+
+        category_chk_query = request.GET.get('category_chk')
+        min_price_query = request.GET.get('min_price')
+
+        if category_chk_query != '' and category_chk_query is not None:
+            products = products.filter(category__name__icontains=category_chk_query)
+        if min_price_query != '' and min_price_query is not None:
+            products = products.filter(price__gte=min_price_query)
+
+
         context = {
             'products': products,
             'categories': categories,
             'season': season,
             'gender': gender,
+            'max_val': max_val.values,
         }
+
+        print(category_chk_query)
+        #get_by_category = products.
+
         return render(request, 'base.html', context)
 
 def filter_view(request):
