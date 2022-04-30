36508124eae4f76a849f89490c8a2be2a914336c
userartem <gumilmo@yandex.ru>
2022-04-30 10:58:15 +0300

    cart prodcuts show and using a shell

--- shop/shopapp/views.py
+++ shop/shopapp/views.py
@@ -97,20 +97,23 @@ class UserViewSet(ListCreateAPIView):
     #filter_backends = (DjangoFilterBackend)
 
     def get_queryset(self):
-        qs = filter(self.request)
+        qs = AnyShoes.objects.all()
+        title = self.request.query_params.get('title')
+        if title is not None:
+            qs = qs.filter(title__icontains=title)
         return qs
     # renderer_classes = [ListCreateAPIView]
     # template_name = 'base.html'
     #
-    def get(self, request):
-        # Получаем набор всех записей из таблицы Capital
-        queryset = AnyShoes.objects.all()
-        # Сериализуем извлечённый набор записей
-        serializer_for_queryset = AnyShoesSerializer(
-            instance=queryset,  # Передаём набор записей
-            many=True  # Указываем, что на вход подаётся именно набор записей
-        )
-        return Response(serializer_for_queryset.data)
+    # def get(self, request):
+    #     # Получаем набор всех записей из таблицы Capital
+    #     queryset = AnyShoes.objects.all()
+    #     # Сериализуем извлечённый набор записей
+    #     serializer_for_queryset = AnyShoesSerializer(
+    #         instance=queryset,  # Передаём набор записей
+    #         many=True  # Указываем, что на вход подаётся именно набор записей
+    #     )
+    #     return Response(serializer_for_queryset.data)
 
 
 # class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
